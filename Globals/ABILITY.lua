12da37d0d670ace21bf1de8ccaa876a1fcfff581
nazarpunk <nazarpunk@ya.ru>
2019-06-19 11:48:55 +0300

    Init

---
+++ Globals/ABILITY.lua
@@ -0,0 +1,40 @@
+ABILITY = {
+	-- 1:ork
+	ork   = {
+		name        = 'Сила орка',
+		id          = FourCC('0000'),
+		icon        = 'ReplaceableTextures\\CommandButtons\\BTNGrunt.blp',
+		description = 'Повышает характеристики орка',
+	},
+	-- 1:ork 1:str
+	smash = {
+		name        = 'Грохот',
+		id          = FourCC('0000'),
+		icon        = 'ReplaceableTextures\\CommandButtons\\BTNUnholyAura.blp',
+		description = 'Герой совершает розящий удар по земле, нанося урон врагам вокруг (без цели)',
+	},
+	jump  = {
+		name        = 'Яростный врыв',
+		id          = FourCC('0000'),
+		icon        = 'ReplaceableTextures\\CommandButtons\\BTNThorns.blp',
+		description = 'Яростно врывается прыжком в указанную область, нанося урон поражённым целям',
+	},
+	helix = {
+		name        = 'Размашистый удар',
+		id          = FourCC('0000'),
+		icon        = 'ReplaceableTextures\\CommandButtons\\BTNCommand.blp',
+		description = 'Совершает резкий размашистый удар вокруг себя, нанося врагам урон',
+	},
+	swipe = {
+		name        = 'Гневный удар',
+		id          = FourCC('0000'),
+		icon        = 'ReplaceableTextures\\CommandButtons\\BTNTrueShot.blp',
+		description = 'Каждый 10-ый удар совершается с особой яростью и наносит дополнительный урон',
+	},
+	rage  = {
+		name        = 'Бешенство',
+		id          = FourCC('0000'),
+		icon        = 'ReplaceableTextures\\CommandButtons\\BTNSilence.blp',
+		description = 'Герой впадает в бешенство, приобретая усиленные рефлексы, а также получает сопротивляемость к магии',
+	}
+}
---
+++ Globals/RACE.lua
@@ -0,0 +1,110 @@
+RACE = {
+	-- 1: ork
+	{
+		name  = "Орк",
+		model = "UI\\Glues\\Singleplayer\\Orccampaign3d\\orccampaign3d.mdx",
+		attr  = {
+			-- 1: str
+			{
+				description = "Орк - cила",
+				unit        = FourCC("O000"),
+				ability     = {
+					ABILITY.smash,
+					ABILITY.jump,
+					ABILITY.helix,
+					ABILITY.swipe,
+					ABILITY.rage,
+					ABILITY.ork
+				}
+			},
+			-- 2: agi
+			{
+				unit        = FourCC("O001"),
+				description = "Орк - ловка",
+				ability     = {
+					ABILITY.ork
+				}
+			},
+			-- 3: int
+			{
+				unit        = FourCC("O002"),
+				description = "Орк - инта",
+				ability     = {
+					ABILITY.ork
+				}
+			}
+		}
+	},
+	--[[
+	-- 2: human
+	{
+		name  = "Человек",
+		model = "UI\\Glues\\Singleplayer\\Humancampaign3d\\humancampaign3d.mdx",
+		attr  = {
+			-- 1: str
+			{
+				unit        = FourCC("Hmkg"),
+				description = "Хуман - сила"
+			},
+			-- 2: agi
+			{
+				unit        = FourCC("Hpal"),
+				description = "Хуман - ловка"
+			},
+			-- 3: int
+			{
+				unit        = FourCC("Hblm"),
+				description = "Хуман - инта"
+			}
+		}
+	},
+	-- 3: undead
+	{
+		name  = "Нежить",
+		model = "UI\\Glues\\Singleplayer\\Undeadcampaign3d\\undeadcampaign3d.mdx",
+		attr  = {
+			-- 1: str
+			{
+				unit        = FourCC("Ucrl"),
+				description = "Андед - сила"
+			},
+			-- 2: agi
+			{
+				unit        = FourCC("Udre"),
+				description = "Андед - ловка"
+			},
+			-- 3: int
+			{
+				unit        = FourCC("Ulic"),
+				description = "Андед - инта"
+			}
+		}
+	},
+	-- 4: elf
+	{
+		name  = "Эльф",
+		model = "UI\\Glues\\Singleplayer\\Nightelfcampaign3d\\nightelfcampaign3d.mdx",
+		attr  = {
+			-- 1: str
+			{
+				unit        = FourCC("Ucrl"),
+				description = "Эльф - сила"
+			},
+			-- 2: agi
+			{
+				unit        = FourCC("Udre"),
+				description = "Эльф - ловка"
+			},
+			-- 3: int
+			{
+				unit        = FourCC("Ulic"),
+				description = "Эльф - инта"
+			}
+		}
+	},
+	]]
+	-- 5
+	{
+		name = "Случайная"
+	}
+}
