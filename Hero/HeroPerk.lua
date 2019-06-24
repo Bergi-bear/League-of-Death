2588c72eba613a1f0be63cab521c271110ef0cb6
nazarpunk <nazarpunk@ya.ru>
2019-06-24 16:49:14 +0300

    perk

--- Hero/HeroPerk.lua
+++ Hero/HeroPerk.lua
@@ -18,10 +18,7 @@ function HeroPerkInit()
 	local BtnText = BlzGetFrameByName('TimerDialogTitle', 0)
 	BlzFrameClearAllPoints(BtnText)
 	BlzFrameSetPoint(BtnText, FRAMEPOINT_CENTER, BtnTextWrap, FRAMEPOINT_CENTER, 0, 0)
-	BlzFrameSetText(BtnText, '12')
-	print(BlzFrameGetHeight(BtnText))
-	BlzFrameSetText(BtnText, '12\n12\n12\n12\n12\n12\n12\n12\n12\n')
-	print(BlzFrameGetHeight(BtnText))
+	BlzFrameSetText(BtnText, '0')
 	
 	local BtnTrigger = CreateTrigger()
 	BlzTriggerRegisterFrameEvent(BtnTrigger, Btn, FRAMEEVENT_CONTROL_CLICK)
@@ -39,6 +36,30 @@ function HeroPerkInit()
 		end
 	end)
 	
+	--> AbilWrap
+	local AbilWrap = BlzCreateFrame('EscMenuBackdrop', GameUI, 0, 0)
+	BlzFrameSetSize(AbilWrap, 0.337, 0.292)
+	BlzFrameSetPoint(AbilWrap, FRAMEPOINT_TOPLEFT, GameUI, FRAMEPOINT_TOPLEFT, 0, -0.05)
+	
+	AbilBtn = {}
+	for i = 1, 6 do
+		AbilBtn[i] = BlzCreateFrame('ScoreScreenBottomButtonTemplate', AbilWrap, 0, 0)
+		BlzFrameSetSize(AbilBtn[i], 0.046, 0.046)
+		BlzFrameSetTexture(BlzGetFrameByName('ScoreScreenButtonBackdrop', 0), 'ReplaceableTextures\\CommandButtons\\BTNAbility1.blp', 0, true)
+		if i == 1 then
+			BlzFrameSetPoint(AbilBtn[i], FRAMEPOINT_TOPLEFT, AbilWrap, FRAMEPOINT_TOPLEFT, 0.03, -0.03)
+		else
+			BlzFrameSetPoint(AbilBtn[i], FRAMEPOINT_LEFT, AbilBtn[i - 1], FRAMEPOINT_RIGHT, 0, 0)
+		end
+	end
+	
+	--> PerkWrap
+	local PerkWrap   = BlzCreateFrame('ListBoxWar3', AbilWrap, 0, 0)
+	BlzFrameSetSize(PerkWrap, 0.273, 0.184)
+	BlzFrameSetPoint(PerkWrap, FRAMEPOINT_TOPLEFT, AbilBtn[1], FRAMEPOINT_BOTTOMLEFT, 0.001, 0)
+	
+	
+	
 	
 	--{ TEST
 	local V          = 0.1
@@ -47,7 +68,7 @@ function HeroPerkInit()
 		V = V + add
 		ClearTextMessages()
 		print(V)
-	
+		BlzFrameSetSize(PerkWrap, 0.273, V)
 	end
 	--} TEST
 	
